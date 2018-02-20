a1de09f04bb9064d0899a4bd54a9957f04948b19
Maricruz <mac@MacBook-Pro-de-mac-2.local>
2018-02-19 22:36:01 -0500

    ejercicio

--- L5UIEvents/app/src/main/java/com/androidbootcamp/starttemplate/l5uievents/MainActivity.java
+++ L5UIEvents/app/src/main/java/com/androidbootcamp/starttemplate/l5uievents/MainActivity.java
@@ -2,12 +2,21 @@ package com.androidbootcamp.starttemplate.l5uievents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signin_layout);
+        Button loginButton = findViewById(R.id.signinButton);
+        EditText UsarnameEditText = findViewById(R.id.idText)
+        loginButton.setOnClickListener new View.OnClickListener(){
+
+
+        }
     }
 }
